4107b27826d1e3645b296636aced29c0f1aaa925
Blurpek <henryssj4@gmail.com>
2020-01-31 00:04:24 +0100

    add few themes

--- react-app/src/ThemeContext.js
+++ react-app/src/ThemeContext.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 export const themes = {
   classic: {
+    name: 'Classic',
+    backgroundColor: {
+      main: '#fffeee',
+      light: '#ffffff',
+      dark: '#eeeeee',
+      contrastColor: ''
+    },
+    textColor: {
+      main: '#333'
+    },
+    primaryColor: '#87ceeb',
+    secondaryColor: '#90EE90',
+    shadow: 'grey'
+  },
+  orangeTeal: {
+    name: 'Orange & Teal',
     backgroundColor: {
       main: '#fffe',
       light: '#ffffff',
@@ -11,8 +27,37 @@ export const themes = {
     textColor: {
       main: '#333'
     },
+    primaryColor: '#12908E',
+    secondaryColor: '#F98F45',
+    shadow: 'grey'
+  },
+  dark: {
+    name: 'Dark',
+    backgroundColor: {
+      main: '#333333',
+      light: '#333444',
+      dark: '#444444'
+    },
+    textColor: {
+      main: '#fffeee'
+    },
     primaryColor: '#87ceeb',
-    secondaryColor: '#90EE90'
+    secondaryColor: '#90EE90',
+    shadow: 'grey'
+  },
+  darkOrangeTeal: {
+    name: 'Dark O&T',
+    backgroundColor: {
+      main: '#333333',
+      light: '#333444',
+      dark: '#444444'
+    },
+    textColor: {
+      main: '#fffeee'
+    },
+    primaryColor: '#12908E',
+    secondaryColor: '#F98F45',
+    shadow: 'grey'
   }
 }
 
