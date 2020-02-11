d201c64aefb1247c4282e14bcdcb1233a74bf097
Blurpek <henryssj4@gmail.com>
2020-02-11 20:20:26 +0100

    add showing images on a card

--- react-app/src/components/CardSideContent/CardSideContent.js
+++ react-app/src/components/CardSideContent/CardSideContent.js
@@ -2,9 +2,15 @@ import React from 'react'
 import './CardSideContent.css'
 
 function CardSideContent({ content }) {
-  return (
+  return content.image ? (
     <div className="flex-container">
-      <h1 className="row" style={{ marginTop: content?.text?.length < 100 ? '30%' : 0 }}>{ content && content.text }</h1>
+      <h1 className="row" style={{  }}>
+        <img src={'data:image/png;base64,' + content.image} />
+      </h1>
+    </div>
+  ) : (
+    <div className="flex-container">
+      <h1 className="row" style={{ marginTop: content?.text?.length < 100 ? '30%' : 0 }}>{ content?.text }</h1>
     </div>
   )
 }
